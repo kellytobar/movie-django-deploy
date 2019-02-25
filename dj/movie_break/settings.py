a5d927bb5966d98867b20220abcdcd18d38ca4fb
kelly tobar <kellytobar7@gmail.com>
2019-02-25 16:45:31 -0500

    settings.py fixed

--- dj/movie_break/settings.py
+++ dj/movie_break/settings.py
@@ -85,10 +85,10 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'movie_break_django',
+        'NAME': 'movie_django_deploy',
         'USER': 'root', 
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
+        'PASSWORD': 'Lab2018*',
+        'HOST': 'db',
         'PORT': '3306',
     }
 }
